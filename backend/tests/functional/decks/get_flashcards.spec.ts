7ac4094cc1d6dc5d95033a1d01a0b9534fb43c63
RemiSaurel <r.saurel5@gmail.com>
2024-09-20 17:22:43 +0200

    test: decks / cards deletion

--- backend/tests/functional/decks/get_flashcards.spec.ts
+++ backend/tests/functional/decks/get_flashcards.spec.ts
@@ -3,6 +3,8 @@ import { DeckFactory } from '#database/factories/deck_factory'
 import User from '#models/user'
 import { test } from '@japa/runner'
 import { FlashcardFactory } from '#database/factories/flashcard_factory'
+import FlashcardsService from '../../../service/flashcards_service.js'
+import ApiBuilderService from '../../../service/api_builder_service.js'
 
 let user: User
 
@@ -45,5 +47,28 @@ test.group('Create / Get / Delete flashcards', (group) => {
     flashcardList.forEach((flashcard, index) => {
       assert.equal(flashcard.front, flashcards[index].front)
     })
+  })
+
+  test('A deleted flashcard should not be found', async ({ assert }) => {
+    const deck = await user.related('decks').query().first()
+    const flashcards = await FlashcardFactory.createMany(3)
+    const flashcardList = await deck?.related('flashcards').createMany(flashcards)
+    const flashcardService = new FlashcardsService(new ApiBuilderService())
+
+    if (!flashcardList || flashcardList.length === 0) {
+      return assert.fail('Flashcards not found')
+    }
+
+    // Delete the first flashcard
+    await flashcardService.deleteFlashcard(user.id, flashcardList[0].id)
+
+    // Get the flashcards
+    const newFlashcardList = await deck?.related('flashcards').query()
+
+    // Make sure the flashcard is not found
+    assert.notEqual(
+      newFlashcardList?.find((flashcard) => flashcard.id === flashcardList[0].id),
+      flashcardList[0]
+    )
   })
 })
