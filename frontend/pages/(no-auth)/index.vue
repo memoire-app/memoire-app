a444bb604e002353f117baf1c578bbb31c5811f6
RemiSaurel <r.saurel5@gmail.com>
2024-10-17 14:00:25 +0200

    feat: better motion :art:

--- frontend/pages/(no-auth)/index.vue
+++ frontend/pages/(no-auth)/index.vue
@@ -53,22 +53,74 @@ watch(
         class="flex w-full flex-col items-center justify-center gap-4 pt-24 text-center md:pt-48"
       >
         <div
-          class="rounded-3xl border border-slate-200 px-3 py-1 text-sm text-slate-700 transition-all dark:text-slate-300"
-          to="/login"
+          v-motion
+          :initial="{ opacity: 0, y: -50 }"
+          :enter="{
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1500, delay: 1000 },
+          }"
         >
-          ✨ {{ t("landing.welcome") }}
+          <div
+            class="rounded-3xl border border-slate-200 px-3 py-1 text-sm text-slate-700 transition-all dark:text-slate-300"
+            to="/login"
+          >
+            ✨ {{ t("landing.welcome") }}
+          </div>
         </div>
-        <h1 class="text-4xl font-bold lg:text-6xl">
+        <h1
+          v-motion
+          :initial="{ opacity: 0, x: -200 }"
+          :enter="{
+            opacity: 1,
+            x: 0,
+            transition: { duration: 1500 },
+          }"
+          class="text-4xl font-bold lg:text-6xl"
+        >
           {{ t("landing.study") }}
         </h1>
-        <h3 class="max-w-[500px] pt-2 text-slate-500">
+        <h3
+          v-motion
+          :initial="{ opacity: 0, x: -200 }"
+          :enter="{
+            opacity: 1,
+            x: 0,
+            transition: { duration: 1500 },
+          }"
+          class="max-w-[500px] pt-2 text-slate-500"
+        >
           {{ t("landing.subtitle") }}
         </h3>
-        <UButton class="mt-8" to="/login" size="xl" icon="i-lucide-badge-plus">
-          {{ t("landing.create") }}
-        </UButton>
+        <div
+          v-motion
+          :initial="{ opacity: 0, y: 50 }"
+          :enter="{
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1500, delay: 1500 },
+          }"
+        >
+          <UButton
+            class="mt-8"
+            to="/login"
+            size="xl"
+            icon="i-lucide-badge-plus"
+          >
+            {{ t("landing.create") }}
+          </UButton>
+        </div>
 
-        <div class="mt-24 hidden md:block">
+        <div
+          v-motion
+          :initial="{ opacity: 0, y: 50 }"
+          :enter="{
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1500, delay: 1500 },
+          }"
+          class="mt-24 hidden md:block"
+        >
           <div
             class="flex h-12 w-7 justify-center rounded-xl border-2 border-slate-400 pt-2"
           >
@@ -92,7 +144,7 @@ watch(
         :visibleOnce="{ opacity: 1, y: 0, scale: 1 }"
         :duration="1500"
       >
-        <div class="mt-48 hidden skew-x-12 justify-center md:flex">
+        <div class="mt-36 hidden skew-x-12 justify-center md:flex">
           <NuxtImg src="/FLASHCARDS.png" alt="hero" class="w-4/5" />
         </div>
       </div>
@@ -117,15 +169,23 @@ watch(
         </span>
 
         <div class="flex w-full flex-col gap-8 lg:w-1/2">
-          <LandingAutoplayBox
+          <div
+            v-motion
             v-for="(feature, i) in FEATURES"
             :key="feature.title"
-            :title="feature.title"
-            :content="feature.content"
-            :index="i + 1"
-            @mouseenter="setActive(i)"
-            @mouseleave="clearActive"
-          />
+            :initial="{ opacity: 0, x: i % 2 === 0 ? 400 : -400 }"
+            :visibleOnce="{
+              opacity: 1,
+              x: 0,
+              transition: { duration: 1500, easing: 'ease-out' },
+            }"
+          >
+            <LandingAutoplayBox
+              :title="feature.title"
+              :content="feature.content"
+              :index="i + 1"
+            />
+          </div>
         </div>
       </UContainer>
 
